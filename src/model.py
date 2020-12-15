9111bd75fc9574e47689c936a49b9c001bbc5bb3
Brock Stechschulte <stechsbj@mail.uc.edu>
2020-12-14 20:56:45 -0500

    Add model.py

--- src/model.py
+++ src/model.py
@@ -1,5 +1,36 @@
+import numpy as np
+import pandas as pd
+from sportsreference.ncaab.teams import Teams
+from sportsreference.ncaab.schedule import Schedule
+
+
 class Model:
 
-    # TODO
     def __init__(self):
-        pass
+        self.df = pd.DataFrame()
+        self.teams = pd.DataFrame()
+
+    # Note: This process takes multiple hours to run
+    def build(self):
+        self.teams = Teams(2020).dataframes['abbreviation']
+        self.pull_team_stats()
+        self.clean_data()
+        self.df.to_excel('model_data.xlsx')
+        self.calculate()
+
+    def pull_team_stats(self):
+        for team in self.teams.head(2):
+            for year in range(2015, 2017):
+                self.df = self.df.append(Schedule(team, year).dataframe_extended)
+
+    def clean_data(self):
+        self.df = self.df.drop(columns=['home_ranking', 'away_ranking', 'date', 'location', 'losing_abbr',
+                                        'winning_abbr', 'losing_name', 'winning_name', 'winner']).dropna()
+
+    def calculate(self):
+        r_squared_array = {}
+        for column in self.df:
+            correlation_matrix = np.corrcoef(self.df['home_points'], self.df[column].astype('float64'))
+            r_squared = correlation_matrix[0, 1] ** 2
+            r_squared_array[column] = r_squared
+        print(sorted(r_squared_array.items(), key=lambda x: x[1], reverse=True))
